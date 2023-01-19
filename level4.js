660a740c0740049f40d926184acf14ff03d40468
fatematuzjohora <fatematuzjohora889@gmail.com>
2023-01-19 13:59:39 +0600

    altering challenge

--- level4.js
+++ level4.js
@@ -135,3 +135,27 @@ function translate(str){
 }
 let reverse = translate('fatema');
 console.log(reverse);
+
+// DNA Pairing For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+function pairElement(str){
+  let output = [];
+  for(let i = 0; str.length; i++){
+    if(str[i] === "A"){
+      output.push(["A", "T"])
+    }
+    if(str[i] === "T"){
+      output.push(["T", "A"])
+    }
+     if(str[i] === "G"){
+      output.push(["G", "C"])
+  }
+   if(str[i] === "C"){
+      output.push(["C", "G"])
+}
+  }
+return output;
+}
+  
+
+let encapsulating = pairElement("GCG");
+console.log(encapsulating);
