d164f635c9e4f8142e471c482268d86e92ec838a
fatematuzjohora <fatematuzjohora889@gmail.com>
2023-01-20 13:10:23 +0600

    sum all prime number

--- level5.js
+++ level5.js
@@ -30,4 +30,22 @@ let cont = convertHtml("Nadia & Sadia & tonni");
 let quotation = convertHtml('Stuff in "quotation marks"')
 console.log(convert, cont, quotation);
 
-//2.
+//2.Sum All Primes
+function sumAllPrimes(num){
+    function isPrime(num){
+        let sqrt = Math.sqrt(num);
+        for (let i = 2; i <=sqrt; i++) {
+            if (num % i === 0) 
+            return false
+        }
+        return true;
+    }
+    let sum = 0;
+    for (let i = 2; i <= num; i++){
+        if(isPrime(i))
+        sum += i;
+    }
+    return sum;
+}
+let ans = sumAllPrimes(977);
+console.log(ans);
