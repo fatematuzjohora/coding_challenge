f542bf989d601a33bb961f18887faa1c88a7a528
fatematuzjohora <fatematuzjohora889@gmail.com>
2023-01-20 18:20:33 +0600

    decimal challenge

--- level5.js
+++ level5.js
@@ -61,6 +61,38 @@ function aVeryBigSum(ar){
 let res = aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]);
 console.log(res);
 
+//4..Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+function plusMinus(arr) {
+    
+    let totalPos=0, totalNe=0, totalZe=0, arrLen = arr.length;
+    
+    for(let value of arr){
+        
+        switch(true){
+           
+           case (value > 0):
+            totalPos++;
+            break;
+            
+            case (value < 0):
+            totalNe++;
+            break;             
+            
+            case (value == 0):
+            totalZe++;
+            break;           
+        }
+        
+    }
+    
+    console.log(`${(totalPos/arrLen).toFixed(6)} \n ${(totalNe/arrLen).toFixed(6)} \n ${(totalZe/arrLen).toFixed(6)}`);
+
+}
+
+let negPos = plusMinus([-4,3,-9,0,4,1]);
+console.log(negPos);
+
+
 //5. Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
 //Write a program that prints a staircase of size .
 function staircase(n) {
