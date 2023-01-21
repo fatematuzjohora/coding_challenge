339a883223d93b84d37220354ef421ad9f67f68e
fatematuzjohora <fatematuzjohora889@gmail.com>
2023-01-21 14:52:13 +0600

    if-else challenge

---
+++ level6.js
@@ -0,0 +1,28 @@
+//1.Conditional Statements: If-Else
+
+function getGrade(score) {
+    let grade;
+    if(25 <score && score <= 30){
+        grade = 'A';
+    }
+    if(20 <score && score <= 25){
+        grade = 'B';
+    }
+    if(15 <score && score <= 20){
+        grade = 'C';
+    }
+    if(10 <score && score <= 15){
+        grade = 'D';
+    }
+    if(5 <score && score <= 10){
+        grade = 'E';
+    }
+    if(0 <= score && score <= 5){
+        grade = 'F';
+    }
+    
+    return grade;
+}
+
+let output = getGrade(30);
+console.log(output);
