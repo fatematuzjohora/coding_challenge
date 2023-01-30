33ce3e5da5848cd5c9ddc1972fc1a62ab2d10afc
fatematuzjohora <fatematuzjohora889@gmail.com>
2023-01-30 10:43:53 +0600

    application challenge

--- practice.js
+++ practice.js
@@ -30,3 +30,38 @@ function gradingStudents(grades) {
     }
     return gradesFinal;
 }
+
+// sales by match hackerrank solution javascript
+function sockMerchant(n, ar) {
+    // Write your code here
+    const uniqueValues = [... new Set(ar)]
+    let count = 0
+    uniqueValues.forEach(value => {
+        const filterValues = ar.filter(element => element == value)
+        const pairsNumber = Math.floor(filterValues.length/2)
+        count += pairsNumber
+    })
+    return count
+}
+
+// counting valleys
+function countingValleys(steps, path) {
+    // Write your code here
+
+    let strArr = path.split('')
+    let count = 0
+    let result = 0
+    for(let step=0; step<steps; step++){
+        if(count == 0 && strArr[step].toLowerCase() == 'd'){
+            count -= 1
+            result += 1
+        } else if(strArr[step].toLowerCase() == 'd'){
+            count -= 1
+        } else {
+            count += 1
+        }
+    }
+    return result
+}
+
+// 
